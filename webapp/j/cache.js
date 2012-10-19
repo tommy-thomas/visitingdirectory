d4d2b695ffe26234dcb920751ea784f4bc0293b9
Tommy Thomas <tommyt@uchicago.edu>
2012-10-19 16:11:27 +0000

    [visdirectory] Changes after client testing.

--- webapp/j/cache.js
+++ webapp/j/cache.js
@@ -1,13 +1,6 @@
 // JavaScript Document
-$(function(){
+(function(){
 	$.ajax({
-		 url: "cache.php",
-		 timeout: 300000
+		 url: "cache.php"
 	  });
-	var timer = $.timer(function() {
-		$.ajax({
-			 url: "cache.php"
-		  });
-	});
-	timer.set({ time : 60000, autostart : true });
 })();
