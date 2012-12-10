1213613a94c9bda2000bcb59031a9af51dc57283
Lynn Barnett <lynnb@uchicago.edu>
2012-12-10 17:56:58 +0000

    fixed title on two pages

--- webapp/_templates/index.html.cs
+++ webapp/_templates/index.html.cs
@@ -2,7 +2,7 @@
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
-		<title>Visiting Committee Directory | Board of Trustees | The University of Chicago</title>
+		<title>Visiting Committees Directory | Board of Trustees | The University of Chicago</title>
 		<?cs include:"inc/head.html.cs" ?>	
 	</head>
 	<body class="two-col-temp">
