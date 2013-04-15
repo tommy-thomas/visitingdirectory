3668d18484ff6b371e7c9b0f369895852d91b675
Lamar Meigs <lmeigs@uchicago.edu>
2013-04-15 19:57:54 +0000

    updating visiting committee link in nav

--- webapp/_templates/inc/navigation.html.cs
+++ webapp/_templates/inc/navigation.html.cs
@@ -2,13 +2,11 @@
 	<div class="content">
 		<ul class="menu ">
 			<li class="first"><a href="http://trustees.uchicago.edu/" class="active">Board of Trustees</a></li>
-			<li class=""><a href="<?cs var:base ?>search.php">Directory Search</a></li>
 			<li class=""><a href="http://vc.uchicago.edu/">Visiting Committees</a>
-				<ul class="menu clearfix">
-					<?cs each:c=Committee ?>
-					<li class="leaf"><a href="<?cs var:base ?>visiting/committee.php?c=<?cs var:c.COMMITTEE_CODE ?>"><?cs var:c.FULL_DESC ?></a></li>
-					<?cs /each ?>					
-				</ul>	
+				<ul class="menu ">
+					<li><a href="/search.php">Directory Search</a></li>
+					<?cs if:LoggedIn ?><li><a href="https://shibboleth2.uchicago.edu/idp/logout.html">Log Out</a></li><?cs /if ?>
+				</ul>				
 			</li>
 			<?cs if:LoggedIn ?><li><a href="https://shibboleth2.uchicago.edu/idp/logout.html">Log Out</a></li><?cs /if ?>
             
