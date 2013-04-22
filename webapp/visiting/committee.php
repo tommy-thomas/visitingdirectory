53271636176492ddaa6bb3d6c500a4a70deef161
Tommy Thomas <tommyt@uchicago.edu>
2013-04-22 15:57:30 +0000

    Fixing bug; needed to create authtoken for non-authenticated users.

--- webapp/visiting/committee.php
+++ webapp/visiting/committee.php
@@ -13,6 +13,9 @@ $template->add_data( "base" , $app->base() );
  */
 
 $curl = new cURL(null);
+$collection = Collection::instance($app , $curl );
+$curl->authenticate( $collection->getLoginUrl() );
+$_SESSION['authtoken'] = array( 'authtoken' => $curl->__toString());
 $collection = Collection::instance( $app , $curl ,  $_SESSION['authtoken']);
 $collection->loadCommitteeTemplateData($template);
 $manager = new CommitteeMemberManager();
