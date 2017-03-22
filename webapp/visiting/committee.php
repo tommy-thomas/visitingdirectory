20ed3bac312097c529a8d29ae273e7faeeb16de7
Tommy Thomas <tommyt@uchicago.edu>
2017-03-22 16:35:28 -0500

    mods for multiple chairs

--- webapp/visiting/committee.php
+++ webapp/visiting/committee.php
@@ -37,6 +37,7 @@ if( isset($_SESSION['authtoken']) && isset($_GET['c']) )
 		$collection->setCachedMemberList($code , $member_list );
 	}	
 	$chair_id = -1;
+	$chairmen = [];
 	foreach( $member_list as $m )
 	{
 		$id_number = $m->getIdNumber();
@@ -44,14 +45,20 @@ if( isset($_SESSION['authtoken']) && isset($_GET['c']) )
 		{
 			$name = $m->getFirstName().' ';
 			$name .= strlen( $m->getMiddleName() ) > 0 ? $m->getMiddleName().' '.$m->getLastName() : $m->getLastName();
-			$name .= ', Chair';				
-			$template->add_data('Chairman', $name );
+			array_push($chairmen , $name);
 			$chair_id = $id_number;
 		}elseif( $id_number != $chair_id )
 		{
 			$m->addClassDataTemplate( $template , "CommitteeMember.$id_number.");	
 		}
 	}
+	$names = "";
+	foreach($chairmen as $key=>$name)
+	{
+		$names .= $name.",";
+	}
+	$names .= count($names) > 1 ? ', Chair' : ', Co-Chairs';
+	$template->add_data('Chairman', $names );
 }
 $template->show();
 ?>
