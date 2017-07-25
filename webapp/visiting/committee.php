a61cfe43d06986bff8a7996bccf0420c0c5289c8
tommy-thomas <tommyt@uchicago.edu>
2017-07-25 14:37:00 -0500

    Removing test code.

--- webapp/visiting/committee.php
+++ webapp/visiting/committee.php
@@ -40,7 +40,6 @@ if( isset($_SESSION['authtoken']) && isset($_GET['c']) )
 	$chairmen = [];
 	foreach( $member_list as $m )
 	{
-		var_dump($m);
 		$id_number = $m->getIdNumber();
 		if( $m->getCommitteeRoleCode() == 'CH' )
 		{
