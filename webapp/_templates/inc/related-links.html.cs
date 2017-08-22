b9a7f3eda86433e3892a7d87d30d84002618a006
tommy-thomas <tommyt@uchicago.edu>
2017-08-22 10:44:47 -0500

    Mod to related links.

--- webapp/_templates/inc/related-links.html.cs
+++ webapp/_templates/inc/related-links.html.cs
@@ -5,7 +5,7 @@
 			<li><a href="//trustees.uchicago.edu">Board of Trustees</a></li>
 			<li><a href="//president.uchicago.edu">Office of the President</a></li>
 			<li><a href="//secretary.uchicago.edu/">Office of the Secretary of the University</a></li>
-			<li><a href="//http://grad.uchicago.edu/admissions/programs">Divisions and Schools</a></li>
+			<li><a href="//grad.uchicago.edu/admissions/programs">Divisions and Schools</a></li>
 		</ul>
 	</div>
 </div>
