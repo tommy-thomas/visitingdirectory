74b58e9c3c8f61953b15a254a121d79584904d17
tommy-thomas <tommyt@uchicago.edu>
2018-05-23 16:38:22 -0500

    Adding old dependencies.

--- webapp/index.php
+++ webapp/index.php
@@ -7,29 +7,31 @@ use \UChicago\AdvisoryCommittee\Application as App;
 
 $app = App::app();
 
-//$template = $app->template('index.html.cs');
-//$template->add_data( "base" , $app->base() );
-//$auth_err = false;
-//$soc_auth_err = false;
-///**
-// * Set committee objects for side nav.
-// */
-//$curl = new cURL(null);
-//$collection = GriffinCollection::instance($app , $curl );
-//$curl->authenticate( $collection->getLoginUrl() );
-//$_SESSION['authtoken'] = array( 'authtoken' => $curl->__toString());
-//$collection->checkCache($_SESSION['authtoken']);
-//$collection->loadCommitteeTemplateData($template);
-//if( $app->isShibbAuth() )
-//{
-//	if( $app->isAuthorized() )
-//	{
-//		$app->redirect('./search.php');
-//	}
-//	elseif( $app->isValidService()  )
-//	{
-//		if( $app->userIsFromSocialAuth() && isset($_SERVER['mail']) )
-//		{
+$auth_err = false;
+$soc_auth_err = false;
+/**
+ * Set committee objects for side nav.
+ */
+
+// TODO: Do all of the validation stuff with Guzzle.
+
+$client = new Client(['base_uri' => 'https://ardapi.uchicago.edu/api/']);
+//$client = new Client(['base_uri' => 'https://ardapi-uat2015.uchicago.edu/api/']); // UAT
+$token = new \UChicago\AdvisoryCommittee\BearerToken($client);
+
+$_SESSION['bearer_token'] = $token->bearer_token();
+
+
+if( $app->isShibbAuth() )
+{
+	if( $app->isAuthorized() )
+	{
+		$app->redirect('./search.php');
+	}
+	elseif( $app->isValidService()  )
+	{
+		if( $app->userIsFromSocialAuth() && isset($_SERVER['mail']) )
+		{
 //		try {
 //			$curl->setPost($_SESSION['authtoken']);
 //			$curl->createCurl( $collection->getServiceUrl('email_validation', $_SERVER['mail'] ) );
@@ -45,45 +47,46 @@ $app = App::app();
 //			} catch (Exception $e) {
 //				Application::handleExceptions($e);
 //			}
-//		}
-//		elseif( $app->userIsFromShibb() )
-//		{
-//			if( !$app->isValidGroup() )
-//			{
-//				$auth_err = true;
-//			}
-//			else
-//			{
-//				$_SESSION['email'] =  $_SERVER['mail'];
-//				$app->redirect('./search.php');
-//			}
-//		}
-//	}
-//	else
-//	{
-//		$soc_auth_err = true;
-//	}
-//}
-//
-///**
-// * Start populating the CS template.
-// * The Clear Silver template.
-// */
-//$template->add_data( "domain" , $app->domain() );
-//$template->add_data( "base" , $app->base() );
-///*
-// * Add authentication error if set.
-// */
-//if( $auth_err || ( isset($_GET['error']) && $_GET['error'] == 'auth') )
-//{
-//	$template->add_data( "authentication_error" , $app->get_error_message(0) );
-//}
-///*
-// * Add soaicl auth error if set.
-// */
-//if( $soc_auth_err )
-//{
-//	$template->add_data( "authentication_error" , $app->get_error_message(1) );
-//}
-//$template->show();
+		}
+		elseif( $app->userIsFromShibb() )
+		{
+			if( !$app->isValidGroup() )
+			{
+				$auth_err = true;
+			}
+			else
+			{
+				$_SESSION['email'] =  $_SERVER['mail'];
+				$app->redirect('./search.php');
+			}
+		}
+	}
+	else
+	{
+		$soc_auth_err = true;
+	}
+}
+
+/**
+ * Start populating the CS template.
+ * The Clear Silver template.
+ */
+$template->add_data( "domain" , $app->domain() );
+$template->add_data( "base" , $app->base() );
+/*
+ * Add authentication error if set.
+ */
+if( $auth_err || ( isset($_GET['error']) && $_GET['error'] == 'auth') )
+{
+	$template->add_data( "authentication_error" , $app->get_error_message(0) );
+}
+/*
+ * Add soaicl auth error if set.
+ */
+if( $soc_auth_err )
+{
+	$template->add_data( "authentication_error" , $app->get_error_message(1) );
+}
+$template->show();
 ?>
+
