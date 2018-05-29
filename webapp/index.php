4a06573efccbf2e5e1c2453b8e256b28a713c49f
tommy-thomas <tommyt@uchicago.edu>
2018-05-29 11:37:25 -0500

    Refactoring index.php

--- webapp/index.php
+++ webapp/index.php
@@ -73,22 +73,29 @@ if( $app->isShibbAuth() )
  * Start populating the CS template.
  * The Clear Silver template.
  */
-$template->add_data( "domain" , $app->domain() );
-$template->add_data( "base" , $app->base() );
-/*
- * Add authentication error if set.
- */
+$template = $app->template("index.html.twig");
+
 if( $auth_err || ( isset($_GET['error']) && $_GET['error'] == 'auth') )
 {
-	$template->add_data( "authentication_error" , $app->get_error_message(0) );
+    $err_msg = $app->get_error_message(0);
 }
 /*
  * Add soaicl auth error if set.
  */
 if( $soc_auth_err )
 {
-	$template->add_data( "authentication_error" , $app->get_error_message(1) );
+    $err_msg = $app->get_error_message(1);
 }
-$template->show();
+
+$template->add_data( "authentication_error" , $app->get_error_message(1) );
+
+echo $template->render([
+
+        "authentication_error" => $err_msg,
+        "domain" , $app->domain(),
+        "base" , $app->base()
+    ]
+);
+
 ?>
 
