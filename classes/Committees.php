db11aa73790fed56390b01f1f42811720c3b32aa
tommy-thomas <tommyt@uchicago.edu>
2018-06-06 14:47:45 -0500

    Mods for membership display.

--- classes/Committees.php
+++ classes/Committees.php
@@ -72,6 +72,7 @@ class Committees
         foreach ($memberships as $key => $committee_code ){
             array_push( $memberships , $this->committees[$committee_code]);
         }
+        var_dump($memberships);
         return $memberships;
     }
 
