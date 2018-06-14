4e4793e34a35a3d4b1829ad53bc7b65f7e012570
tommy-thomas <tommyt@uchicago.edu>
2018-06-14 15:35:53 -0500

    Refactor for creds.

--- classes/Application.php
+++ classes/Application.php
@@ -100,8 +100,6 @@ class Application extends \WS\SharedPHP\WS_Application
     public function apiCreds()
     {
         require ('/data/credentials/visitingdirectory/authorization.php');
-        $username = $USERNAME;
-        $password = $PASSWORD;
         return array("username" => $username,  "password" => $password);
     }
 
