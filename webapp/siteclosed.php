cb35b2e671737cb265c9357ab9909b9e62ab3627
Tommy <tommyt@uchicago.edu>
2023-04-24 10:24:26 -0500

    Adding a site closed page.

--- webapp/siteclosed.php
+++ webapp/siteclosed.php
@@ -1,5 +1,5 @@
 <?php
-require('../classes/autoload.php');
+require __DIR__ . "/../vendor/autoload.php";
 $app = new Application;
 $twig = $app->template('siteclosed.twig');
 echo $twig->render([]);
