441b70384ffcd9bbe82f2d30739c42b801160e72
Tommy <tommyt@uchicago.edu>
2023-04-25 21:31:19 -0500

    Testing static cached data.

---
+++ classes/Data/StaticRepository.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tommy-thomas
+ * Date: 5/30/18
+ * Time: 2:34 PM
+ */
+
+namespace UChicago\AdvisoryCouncil\Data;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Response;
+use UChicago\AdvisoryCouncil\CLIMemcache;
+use UChicago\AdvisoryCouncil\CommitteeMemberFactory;
+use UChicago\AdvisoryCouncil\CommitteeMemberMembership;
+use UChicago\AdvisoryCouncil\Committees;
+
+class StaticRepository
+{
+    private $data = array();
+    private $bearer_token;
+    private $client;
+    private $memcache;
+
+    public function __construct(CLIMemcache $memcache, $environment = "dev")
+    {
+        $this->memcache = $memcache;
+
+        if (!$this->memcache->get('AdvisoryCouncilsMemberData') || !$this->memcache->get('AdvisoryCouncilsMemberMembershipData')) {
+            $this->setData();
+        }
+    }
+
+    public function setData()
+    {
+        //set data array
+        $this->data['AdvisoryCouncilsMemberData'] = $this->memcache->get('AdvisoryCouncilsMemberData');
+        $this->data['AdvisoryCouncilsMemberMembershipData'] = $this->memcache->get('AdvisoryCouncilsMemberMembershipData');
+    }
+
+    public function getCouncilData($code)
+    {
+        if (isset($this->data['AdvisoryCouncilsMemberData'][$code])) {
+            return $this->data['AdvisoryCouncilsMemberData'][$code];
+        }
+        return array();
+    }
+
+    public function findMemberByIdNumber($id_number = "" ){
+        foreach ( $this->data['AdvisoryCouncilsMemberData'] as $key => $committee){
+           foreach ( $committee as $member ){
+               if( $member->id_number() == $id_number ){
+                   return $member;
+               }
+           }
+        }
+        return;
+    }
+
+    public function allCouncilData()
+    {
+        return $this->data['AdvisoryCouncilsMemberData'];
+    }
+
+    public function getCouncilMembershipData()
+    {
+        if (isset($this->data['AdvisoryCouncilsMemberMembershipData']['committee_membership'])) {
+            return $this->data['AdvisoryCouncilsMemberMembershipData']['committee_membership'];
+        }
+        return new CommitteeMemberMembership();
+    }
+}
---
+++ webapp/index.php
@@ -0,0 +1,57 @@
+<?php
+require __DIR__ . "/../vendor/autoload.php";
+
+
+$app = new UChicago\AdvisoryCouncil\Application;
+
+$auth_err = false;
+$valid_social_auth = null;
+
+/**
+ * Set committee objects for side nav.
+ */
+
+if ( $app->isAuthorized() ) {
+        $app->redirect('./search.php');
+}
+
+//Adding to test security scan header
+if( $app->isAppSecScan() ){
+    $_SESSION['email'] = 'oregonian@alumni.uchicago.edu';
+    $app->redirect('./search.php');
+}
+
+if (  $app->userIsFromShibb() && $app->isValidGroup() ) {
+    $_SESSION['email'] = $_SERVER['mail'];
+    $app->redirect('./search.php');
+}
+
+if ($app->userIsFromShibb() && !$app->isValidGroup() ) {
+    $auth_err = true;
+}
+
+/**
+ * Start Twig
+ */
+$template = $app->template("index.html.twig");
+
+$err_msg = "";
+if ($auth_err || (isset($_GET['error']) && $_GET['error'] == 'auth')) {
+    $err_msg = $app->getErrorMessage(0);
+}
+/*
+ * Add social auth error if set.
+ */
+if ( isset($valid_social_auth) && !is_null($valid_social_auth) && !$valid_social_auth) {
+    $err_msg = $app->getErrorMessage(1);
+}
+
+echo $template->render([
+
+        "authentication_error" => $err_msg,
+        "domain" => $app->domain(),
+    ]
+);
+
+?>
+
