0083d50a1d7bb461ac2f477ed3f1b94643b52b2b
Tommy <tommyt@uchicago.edu>
2023-07-11 15:31:54 -0500

    Testing cache file.

--- webapp/cache/cache.php
+++ webapp/cache/cache.php
@@ -2,7 +2,10 @@
 
 require __DIR__ . "/../../vendor/autoload.php";
 use GuzzleHttp\Client;
+use use UChicago\AdvisoryCouncil\Application;
 use UChicago\AdvisoryCouncil\Data\Repository;
 
 $app = new Application();
 $client = new Client();
+$repo = new Repository($client, $app->apiUrl());
+$repo->cache();
