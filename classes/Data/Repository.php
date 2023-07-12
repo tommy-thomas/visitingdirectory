5b99644cf5c1f8a0c591ea755e51c301d8e7e125
Tommy <tommyt@uchicago.edu>
2023-07-12 09:07:54 -0500

    Clean up...

--- classes/Data/Repository.php
+++ classes/Data/Repository.php
@@ -36,7 +36,7 @@ class Repository
     public function __construct(Client $client = null, $uri = null, $environment = "dev")
     {
         $this->database = new Database();
-        // If no $client or api uri then instance read only status...
+        // If no $client or api uri then instance is in read only status...
         if (!is_null($client) && !is_null($uri)) {
             $this->committees = new Committees();
             $this->factory = new CommitteeMemberFactory();
