8e6854ac9afe3295d47a22a00e1278dc580618fc
Rose X=Pezzuti Dyer <pezzutidyer@uchicago.edu>
2024-05-15 15:52:47 -0500

    a little more cleanup

--- webapp/index.php
+++ webapp/index.php
@@ -4,42 +4,4 @@ require __DIR__ . "/../vendor/autoload.php";
 
 $app = new UChicago\AdvisoryCouncil\Application;
 
-$auth_err = false;
-$valid_social_auth = null;
-
-
-if ( $app->authorized() ) {
-        $app->redirect('./search.php');
-}
-
-//Adding to test security scan header
-if( $app->isAppSecScan() ){
-    $_SESSION['email'] = 'oregonian@alumni.uchicago.edu';
-    $app->redirect('./search.php');
-}
-
-/**
- * Start Twig
- */
-$template = $app->template("index.html.twig");
-
-$err_msg = "";
-if ($auth_err || (isset($_GET['error']) && $_GET['error'] == 'auth')) {
-    $err_msg = $app->getErrorMessage(0);
-}
-/*
- * Add social auth error if set.
- */
-if ( isset($valid_social_auth) && !is_null($valid_social_auth) && !$valid_social_auth) {
-    $err_msg = $app->getErrorMessage(1);
-}
-
-echo $template->render([
-
-        "authentication_error" => $err_msg,
-        "domain" => $app->domain(),
-    ]
-);
-
-?>
-
+$app->redirect('./search.php');
